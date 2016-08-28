ede45e0b4c6409f1e7360df9a4cac9960a2fe19e
violetaria <terri.chu@outlook.com>
2016-08-28 15:00:36 -0400

    update main

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,24 @@
+Parse.serverURL = 'http://pensumapi.herokuapp.com/parse';
 
 Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+  	res.success('Hi');
+});
+
+Parse.Cloud.afterDelete("Task", function(request) {
+  	query = new Parse.Query("Conversation");
+  	query.equalTo("task", request.object.id);
+  	query.find({
+    	success: function(conversations) {
+        	Parse.Cloud.useMasterKey();
+      		Parse.Object.destroyAll(conversations, {
+        		success: function() {},
+        		error: function(error) {
+          			console.error("Error deleting Task related conversations " + error.code + ": " + error.message);
+        		}
+      		});
+    	},
+    	error: function(error) {
+      		console.error("Error finding Task related conversations " + error.code + ": " + error.message);
+    	}
+  	});
 });
